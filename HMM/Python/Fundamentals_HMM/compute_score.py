5f7c10e8ad602c586369d507748208fb45995255
machiobrian <machiobrian29@gmail.com>
2023-01-09 01:24:59 +0300

    AssertionError: The probabilities must sum upto 1 class ProbabilityVector

--- HMM/Python/Fundamentals_HMM/compute_score.py
+++ HMM/Python/Fundamentals_HMM/compute_score.py
@@ -8,12 +8,15 @@ our known model lambda
 
 we are computing the score naively since - calculate the probability for every possible 
 chain X.
+
+Q - latent states
+O - possible observation states
 """
 
 from itertools import product
 from functools import reduce
 
-from Prob_Matrix import ProbabilityMatrix
+from Prob_Matrix import ProbabilityMatrix, ProbabilityVector
 import numpy as np 
 
 class HiddenMarkovChain:
@@ -110,3 +113,73 @@ class HiddenMarkovChain_Uncover(HiddenMarkovChain_Simulation):
         betas = self._betas(observations)
         maxargs = (alphas*betas).argmax(axis=1)
         return list(map(lambda x: self.states[x], maxargs))
+
+class HiddenMarkovLayer(HiddenMarkovChain_Uncover):
+    # digamma method, performes all neccesary calculations
+    def _digammas(self, observations: list) -> np.ndarray:
+        L, N = len(observations), len(self.states)
+        digammas = np.zeros((L-1,N,N))
+
+        alphas = self._alphas(observations)
+        betas = self._betas(observations)
+        score = self.score(observations)
+
+        for t in range(L-1):
+            P1 = (alphas[t,:].reshape(-1,1)* self.T.values)
+            P2 = self.E[observations[t+1]].T * betas[t+1].reshape(1,-1)
+            digammas[t, :, :] = P1*P2 / score
+            return digammas
+# --------------------------------------------------------------------------- #
+# Model Training Summary:
+# Intialize A,B,pi
+# Calculate gamma(i,j)
+# Update models A,B,pi
+# Repeat 2/3 until the score no longer increases
+
+class HiddenMarkovModel:
+    def __init__(self, hml: HiddenMarkovLayer):
+        self.layer = hml
+        self._score_init = 0
+        self.score_history = []
+
+    @classmethod
+    def initialize(cls, states: list, observables:list):
+        layer = HiddenMarkovLayer.initialize(states, observables)
+        return cls(layer)
+    
+    def update(self, observations:list) -> float:
+        alpha = self.layer._alphas(observations)
+        beta = self.layer._betas(observations)
+        digamma = self.layer._digammas(observations)
+        score = alpha[-1].sum()
+        gamma = (alpha*beta)/score
+
+        L = len(alpha)
+        obs_idx = [self.layer.observables.index(x) for x in observations]
+        capture = np.zeros((L, len(self.layer.states), len(self.layer.observables)))
+        for t in range(L):
+            capture[t, :, obs_idx[t]] = 1.0
+        pi = gamma[0]
+        T = digamma.sum(axis=0) / gamma[:-1].sum(axis=0).reshape(-1,1)
+        E = (capture*gamma[:,:,np.newaxis]).sum(axis=0)/gamma.sum(axis=0).reshape(-1,1)
+        
+        self.layer.pi = ProbabilityVector.from_numpy(pi, self.layer.states)
+        self.layer.T = ProbabilityMatrix.from_numpy(T, self.layer.states, self.layer.states)
+        self.layer.E = ProbabilityMatrix.from_numpy(E, self.layer.states, self.layer.observables)
+        return score
+    
+    def train(self, observations: list, epochs:int, tol=None):
+        self._score_init = 0
+        self.score_history = (epochs + 1) * [0]
+        early_stopping = isinstance(tol, (int, float))
+
+        for epoch in range(1, epochs+1):
+            score = self.update(observations)
+
+            print("Training ... epoch = {} out of {}, score = {}".format(epoch, epochs, score))
+
+            if early_stopping and abs(self._score_init - score)/ score<tol:
+                print('Early stopping.')
+                break
+            self._score_init = score
+            self.score_history[epoch] = score
